e32a3642c8c31b719b2a734ea20c5ba7e6e42009
francislouie <francislouie.delrosario@gmail.com>
2018-02-24 00:25:18 +0800

    incoming and outgoing report, outgoing recipient not done yet

--- __manifest__.py
+++ __manifest__.py
@@ -28,6 +28,9 @@
         # 'views/views.xml',
         # 'views/templates.xml',
         'reports/reports_handler_view.xml',
+        'reports/in_out_report_view.xml',
+        'reports/templates/incoming_report_templates.xml',
+        'reports/templates/outgoing_report_templates.xml',
     ],
     # # only loaded in demonstration mode
     # 'demo': [
